b7f3eeaada3a77469e131d1da1deae77e3a5f656
Sander Lepik <sander.lepik@knowit.ee>
2017-06-30 14:55:17 +0300

    cleaned code and added some documentation

--- stockInformation/models.py
+++ stockInformation/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Stocks(models.Model):
     symbol = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     change = models.CharField(max_length=200)
 
     def __str__(self):
--- stockInformation/views.py
+++ stockInformation/views.py
@@ -6,25 +6,41 @@ from .forms import StockForm
 
 
 def update_stock_table(request):
+    """
+    function, that allows to add or remove stock options and displays them in portfolio
+    
+    :param request: POST request that defines weather to add or remove stock options to portfolio
+    :return: index.html with context dictionary that has all the stock options that you have added to portfolio
+    """
+
     stock_list = Stocks.objects.order_by('price')[:10]
 
     today_date = time.strftime("%d.%m.%Y %H:%M")
 
     if 'new_stock' in request.POST:
+
         if request.method == 'POST':
             form = StockForm(request.POST)
+
             if form.is_valid():
                 new_stock = request.POST.get("new_stock", "")
 
-                if Stocks.objects.filter(symbol=new_stock.upper()).count() < 1:
+                if not Stocks.objects.filter(symbol=new_stock.upper()):
+
                     try:
                         stock_object = Share(new_stock)
                         new_stock_name = stock_object.get_name()
                         new_stock_price = stock_object.get_price()
                         new_stock_change = stock_object.get_change()
-                        stock_to_db = Stocks(symbol=new_stock.upper(), name=new_stock_name, price=new_stock_price, change=new_stock_change)
+
+                        stock_to_db = Stocks(symbol=new_stock.upper(),
+                                             name=new_stock_name,
+                                             price=new_stock_price,
+                                             change=new_stock_change)
                         stock_to_db.save()
+
                         add_success_message = "Stock successfully added to portfolio!"
+
                         context = {
                             'stock_list': stock_list,
                             'today_date': today_date,
@@ -34,20 +50,22 @@ def update_stock_table(request):
                     except Exception:
                         pass
                         error_message = "Insert correct symbol!"
+
                         context = {
                             'stock_list': stock_list,
                             'today_date': today_date,
                             'error_message': error_message,
                         }
+
                 else:
                     stock_exists_message = "Stock is already in your portfolio!"
+
                     context = {
                         'stock_list': stock_list,
                         'today_date': today_date,
                         'stock_exists_message': stock_exists_message,
                     }
                     return render(request, 'stockInformation/index.html', context)
-
 
         else:
             for stock in stock_list:
@@ -68,11 +86,13 @@ def update_stock_table(request):
 
         if request.method == 'POST':
             symbol = request.POST.get("remove_stock", "")
+
             try:
-                Stocks.objects.filter(symbol=symbol).delete() # ei saa praegu exceptionit ilmselt
+                Stocks.objects.filter(symbol=symbol).delete()
                 stock_list = Stocks.objects.order_by('price')[:10]
 
                 delete_success_message = "Stock successfully removed from portfolio!"
+
                 context = {
                     'stock_list': stock_list,
                     'today_date': today_date,
@@ -80,8 +100,8 @@ def update_stock_table(request):
                 }
 
             except Exception:
-                # pass
                 remove_error_message = "Insert correct symbol!"
+
                 context = {
                     'stock_list': stock_list,
                     'today_date': today_date,
